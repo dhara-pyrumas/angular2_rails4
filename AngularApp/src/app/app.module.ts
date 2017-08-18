109a3aa26eaba9af844ae888f2e0d5c55277cde8
Dhara Joshi <dhara@pyrumas.com>
2017-08-18 14:57:10 +0530

    finished basic routing

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { HttpModule } from '@angular/http';
-
 import { MaterialModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
+
+import { appRoutes } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { CircularComponent } from './circular/circular.component';
@@ -15,7 +17,12 @@ import { SearchComponent } from './search/search.component';
     CircularComponent,
     SearchComponent
   ],
-  imports: [BrowserModule, HttpModule, MaterialModule],
+  imports: [
+	  RouterModule.forRoot(appRoutes),
+	  BrowserModule,
+	  HttpModule,
+	  MaterialModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
